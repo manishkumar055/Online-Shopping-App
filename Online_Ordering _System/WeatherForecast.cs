6c5e18e5075298661a5b75091ae83f78b9a8122b
Manish Kumar <manishk51063@gmail.com>
2023-03-10 11:49:07 +0530

    Commented out testinggit propety

--- Online_Ordering _System/WeatherForecast.cs
+++ Online_Ordering _System/WeatherForecast.cs
@@ -8,6 +8,7 @@ namespace Online_Ordering__System
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        //public string? Summary { get; set; }
+        public string? Summary { get; set; }
+        //public int testinggit  { get; set; }
     }
 }
